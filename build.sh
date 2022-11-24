a6940a03c4ad7761d474c329e70e1f887673cf14
zacky <103025109+zacky46@users.noreply.github.com>
2022-11-24 13:06:43 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-make banana -j8  > reading #& sleep 95m # Jangan di hapus text line (> reading)
+mka banana -j8  > reading #& sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
