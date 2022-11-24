dabb9e787fd7b47feb473593801d84c24e4a3eb5
zacky <103025109+zacky46@users.noreply.github.com>
2022-11-24 13:08:46 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,15 +13,12 @@ export KBUILD_BUILD_USER=zacky
 export KBUILD_BUILD_HOST=android-build
 export BUILD_USERNAME=zacky
 export BUILD_HOSTNAME=android-build
-export ALLOW_MISSING_DEPENDENCIES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
-export USE_CCACHE=1
 lunch banana_whyred-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-mka banana -j8  > reading #& sleep 95m # Jangan di hapus text line (> reading)
+m banana -j8  > reading #& sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
